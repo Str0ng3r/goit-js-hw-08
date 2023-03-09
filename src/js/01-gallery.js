c77090dc01476d26198901572968f782a43ddd08
Str0ng3r <116167012+Str0ng3r@users.noreply.github.com>
2023-03-10 00:30:53 +0100

    UPDATE 1quest

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,20 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+console.log(galleryItems);
+
+const ulGallery = document.querySelector('.gallery')
+console.log(ulGallery)
+
+galleryItems.forEach(({preview,original,description}) => {
+    ulGallery.insertAdjacentHTML('beforeend',`<div class="gallery__item"><a href=${original} class="gallery__link"><img class="gallery__image" src=${preview} data-caption=${description} data-source=${original} alt=${description}></a></div>`)
+});
+
+let lightbox = new SimpleLightbox('.gallery a', {captions: true,
+    captionSelector: 'img',captionsType:'text',captionsData:'alt',captionDelay:250});
