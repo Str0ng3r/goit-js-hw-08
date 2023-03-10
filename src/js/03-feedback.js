9883b693188f478f8b5d4c90cf578de66980171e
Str0ng3r <116167012+Str0ng3r@users.noreply.github.com>
2023-03-10 23:56:55 +0100

    TOP4IK

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+let throttle = require('lodash.throttle');
 
+'use strict'
+const form = document.getElementsByClassName('feedback-form')
+
+const inputEmail = document.getElementById('email')
+
+const inputMessage = document.getElementById('message')
+
+ const butSubmit = document.getElementById('submitBut')
+ console.log(butSubmit)
+console.log(inputEmail)
+console.log(inputMessage)
+console.log(form)
+
+
+const updateEmail = () => {
+    const valueEmail = inputEmail.value
+    console.log(valueEmail)
+    localStorage.setItem('valEmail', valueEmail)
+}
+ const thrUpdateEmail = throttle(updateEmail,1000);
+inputEmail.addEventListener('input',thrUpdateEmail);
+
+const updateMessage = () => {
+    const valueMess = inputMessage.value
+    localStorage.setItem('message',valueMess)
+}
+
+ const thrUpdateMessage = throttle(updateMessage,1000)
+inputMessage.addEventListener('input',thrUpdateMessage)
+
+
+ if(localStorage.getItem('valEmail') && localStorage.getItem('message')){
+
+
+inputEmail.value = localStorage.getItem('valEmail')
+inputMessage.value = localStorage.getItem('message')
+
+ }
+
+ butSubmit.addEventListener('click',(event) => {
+    event.preventDefault();
+    inputEmail.value = ''
+    inputMessage.value = ''
+ })
